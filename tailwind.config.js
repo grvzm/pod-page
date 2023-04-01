f1521d214f12a9ad83aaa0742cee63aaeb010d6a
thegolumehra <golucodes@gmail.com>
2023-04-01 11:27:24 +0530

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors:{
+        primary:"#ED7F39",
+      },
+      fontFamily:{
+        poppins:['Poppins',"sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
